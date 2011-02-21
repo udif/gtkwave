f95b4a33e7ff17786cb2c4c4b6619970c094164c
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2011-02-21 22:36:22 +0000

    length = 0 fix on FST_VT_VCD_EVENT type

--- gtkwave3/src/helpers/vcd2fst.c
+++ gtkwave3/src/helpers/vcd2fst.c
@@ -261,6 +261,7 @@ while(!feof(f))
 			{
 			case FST_VT_VCD_PORT: len = (len * 3) + 2; break;
 			case FST_VT_GEN_STRING: len = 0; break;
+			case FST_VT_VCD_EVENT: len = (len != 0) ? len : 1;  break;
 			default: break;
 			}
 
