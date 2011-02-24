ceb83fcf6bdae840df8e6904cf70d1de39e8a501
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2011-02-24 21:12:55 +0000

    version bump

--- gtkwave3/src/version.h
+++ gtkwave3/src/version.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) Tony Bybell 2010
+ * Copyright (c) Tony Bybell 2010-2011.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -10,6 +10,6 @@
 #ifndef WAVE_VERSION_H
 #define WAVE_VERSION_H
 
-#define WAVE_VERSION_INFO "GTKWave Analyzer v3.3.20_pre (w)1999-2011 BSI"
+#define WAVE_VERSION_INFO "GTKWave Analyzer v3.3.20 (w)1999-2011 BSI"
 
 #endif
