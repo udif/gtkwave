05cc0457a7e27f1d5d56cdc0681e48fefff3847f
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2011-04-28 20:53:21 +0000

    version bump

--- gtkwave3/src/ae2.h
+++ gtkwave3/src/ae2.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) Tony Bybell 2004.
+ * Copyright (c) Tony Bybell 2004-2011.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
