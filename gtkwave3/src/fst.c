cd30d56232fb2e0a64ac88308c42a321d5a90b00
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2011-05-09 03:41:01 +0000

    comments added

--- gtkwave3/src/fst.c
+++ gtkwave3/src/fst.c
@@ -732,7 +732,7 @@ free_2(f_name_len); f_name_len = NULL;
 
 if(numvars != GLOBALS->numfacs) { GLOBALS->mvlfacs_fst_rvs_alias = realloc_2(GLOBALS->mvlfacs_fst_rvs_alias, numvars * sizeof(fstHandle)); }
 
-decorated_module_cleanup();
+decorated_module_cleanup(); /* ...also now in gtk2_treesearch.c */
 freeze_facility_pack();
 iter_through_comp_name_table();
 
