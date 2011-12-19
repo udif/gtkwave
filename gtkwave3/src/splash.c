003ce2f95b1c4c6b05574a60761cfb9c09ca7521
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2011-12-19 21:18:07 +0000

    disable loader message in all OSs currently because of DnD crash

--- gtkwave3/src/splash.c
+++ gtkwave3/src/splash.c
@@ -775,7 +775,7 @@ if((!GLOBALS->splash_disable)&&(!GLOBALS->splash_splash_c_1))
 	else
 	{
 /* commented out for now because of DnD while loading crash */
-#ifndef MAC_INTEGRATION
+#if 0
 	if(GLOBALS->mainwindow)
 		{
 		wave_gtk_window_set_title(GTK_WINDOW(GLOBALS->mainwindow), GLOBALS->winname, WAVE_SET_TITLE_LOADING, 0);
@@ -808,7 +808,7 @@ if(GLOBALS->splash_splash_c_1)
 	else
 	{
 /* commented out for now because of DnD while loading crash */
-#ifndef MAC_INTEGRATION
+#if 0
 	if(GLOBALS->mainwindow)
 		{
 		if(!GLOBALS->splash_is_loading)
