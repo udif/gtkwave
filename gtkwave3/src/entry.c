d4b6562b550447330f4ae74ce9a4e2a3ede7a33d
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2012-01-25 23:29:14 +0000

    update copyright

--- gtkwave3/src/entry.c
+++ gtkwave3/src/entry.c
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) Tony Bybell 1999-2009.
+ * Copyright (c) Tony Bybell 1999-2012.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
