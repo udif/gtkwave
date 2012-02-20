719bef16cd901393c1cb4ad974edffa1f9829c89
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2012-02-20 21:41:09 +0000

    propagate version numbers from configure.ac

--- gtkwave3/distro_from_svn.sh
+++ gtkwave3/distro_from_svn.sh
@@ -4,7 +4,7 @@ find . | grep '\.svn' | tac | awk '{print "rm -rf "$0}' | sh
 
 echo "Making distribution tarball from SVN directory..."
 cd ../
-cat gtkwave3/src/version.h | grep WAVE_VERSION_INFO | sed 's/.*Analyzer v//' | sed 's/ .*//' | \
+cat ./gtkwave3/configure.ac | grep AC_INIT | awk '{print $2}' | sed 's/,//' | \
 	awk '{print "mv gtkwave3 gtkwave-"$0" ; tar cvf gtkwave-"$0".tar gtkwave-"$0" ; gzip -9 gtkwave-"$0".tar"}' | sh
 
 echo "Done!"
