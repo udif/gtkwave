d458c981e09ef71efb692590b972150f2e19353f
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2012-02-20 18:47:44 +0000

    regenerate in linux for 3.3.33 (future)

--- gtkwave3/src/version.h
+++ gtkwave3/src/version.h
@@ -10,6 +10,6 @@
 #ifndef WAVE_VERSION_H
 #define WAVE_VERSION_H
 
-#define WAVE_VERSION_INFO "GTKWave Analyzer v3.3.32 (w)1999-2012 BSI"
+#define WAVE_VERSION_INFO "GTKWave Analyzer v3.3.33 (w)1999-2012 BSI"
 
 #endif
