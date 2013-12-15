919696e53ec234015584d49d6361dc19591aff35
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2013-12-15 19:12:08 +0000

    update documentation

--- gtkwave3/src/helpers/vcd2fst.c
+++ gtkwave3/src/helpers/vcd2fst.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2013 Tony Bybell.
+ * Copyright (c) 2009-2014 Tony Bybell.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
