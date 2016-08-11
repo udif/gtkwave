62df9e7440538075d5964afadef404327029e101
gtkwave <gtkwave@c588eaa8-7e31-457f-9967-08619db48f1a>
2016-08-11 19:33:49 +0000

    --disable-tcl fix

--- gtkwave3/src/main.c
+++ gtkwave3/src/main.c
@@ -2484,7 +2484,11 @@ if(1)	/* here in order to calculate window manager delta if present... window is
 
 init_busy();
 
-if(scriptfile && GLOBALS->interp)
+if(scriptfile
+#if defined(HAVE_LIBTCL)
+        && GLOBALS->interp
+#endif
+)
 	{
 	execute_script(scriptfile, 1); /* deallocate the name in the script because context might swap out from under us! */
 	scriptfile=NULL;
